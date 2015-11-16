79e69cb03149bf8cca202b88a792d2f7ebbe9b0b
Ahmad N. F <fahmadnaufal@gmail.com>
2015-11-16 09:16:32 +0700

    added csv read

---
+++ src/zooclassificator/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zooclassificator;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Ahmad Naufal Farhan
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ZooClassificator zc = new ZooClassificator("zoo.class");
+    }
+}
