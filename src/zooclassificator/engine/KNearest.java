34307cbfdb88ee3a9da3485a9617e39ff15ca1ef
fauzanriff <fauzanriff@gmail.com>
2015-11-20 20:56:47 +0700

    KNearest Finish

--- src/zooclassificator/engine/KNearest.java
+++ src/zooclassificator/engine/KNearest.java
@@ -32,7 +32,8 @@ public class KNearest {
         for(Data eachdata : dataset.getDataSet()){
             sortedData.add(eachdata);
         }
-        int sizeData = dataset.getDataSet().size();
+        //out.println(sortedData);
+        int sizeData = n;
         int nAttr = dataset.getAttrLib().size();
         int nLastAttr = dataset.getAttrLib().get(nAttr-1).getAttributes().size();
         int[] i = new int[nLastAttr];
@@ -42,11 +43,12 @@ public class KNearest {
         /* Itung nilai attribut terakhir untuk hasil data yang telah di sort */
         for(int j=0; j<sizeData; j++){
             for(int k=0; k<nLastAttr; k++){
-                out.println(nLastAttr);
-                /*if(dummy.getAttributes().get(nAttr-1).equals(dataset.getAttrLib().get(nLastAttr-1).getAttributes().get(k))){
+                
+                if(dataset.getAttrLib().get(nAttr-1).getAttributes().get(k).equals(sortedData.peek().getAttributes().get(nAttr-1))){
                     i[k]++;
-                }*/
+                }
             }   
+            sortedData.poll();
         }
         
         /* Cari yang paling banyak */
@@ -56,8 +58,12 @@ public class KNearest {
                 indMax = j;
             }
         }
-        
-        return dataset.getAttrLib().get(nLastAttr).getAttributes().get(indMax);
+        freeData();
+        return dataset.getAttrLib().get(nAttr-1).getAttributes().get(indMax);
+    }
+    
+    public void freeData(){
+        sortedData.clear();
     }
     
     private class KNNComparator implements Comparator<Data>{
@@ -72,16 +78,66 @@ public class KNearest {
             return 0;
         }
     }
-    private int theValue(Data a, Data b){
-        int value = 0,i = 0;
-        for(String attr : a.getAttributes()){
-            if(attr == b.getAttributes().get(i)){
+    private int theValue(Data b, Data a){
+        int value = 0;
+        int size = b.getAttributes().size()-1;
+        for(int j=0; j<size; j++){
+            if(a.getAttributes().get(j).equals(b.getAttributes().get(j))){
                 //do nothing
             }else{
                 value++;
             }
-            i++;
         }
         return value;
     }
+    public void FullTraining(Pair datasett){
+        double rightValue = 0;
+        for(int i = 0; i < datasett.getDataSet().size(); i++){
+            Pair dataset = datasett;
+            Data data = dataset.getDataSet().get(i);
+            int size = data.getAttributes().size();
+            if(Classificate(dataset, data).equals(data.getAttributes().get(size-1))){
+                rightValue = rightValue + 1;
+            }
+        }
+        out.println(rightValue/datasett.getDataSet().size());
+    }
+    public void tenFold(Pair dataset){
+        double rightValue = 0;
+        int dataSize = dataset.getDataSet().size();
+        int[] arrTemp = new int[10];
+        int retDiv = dataSize/10;
+        int retMod = dataSize%10;
+        for(int j=0; j<10; j++){
+            int plus=0;
+            if(retMod > 0){
+                plus++;
+                retMod--;
+            }
+            arrTemp[j] = retDiv + plus;
+        }
+        int checker=0;
+        int stDel = 0;
+        for(int i = 0; i<dataset.getDataSet().size(); i++){
+            Pair temp = new Pair(dataset);
+            Data data = temp.getDataSet().get(i);
+            int fnDel = arrTemp[checker]-1;
+            
+            if(i > fnDel&&checker<9){
+                stDel += arrTemp[checker];
+                checker++;
+                fnDel += arrTemp[checker];
+            }
+            for(int t=stDel; t<=fnDel; t++){
+                temp.getDataSet().remove(t);
+            }
+            
+            int size = data.getAttributes().size();
+            if(Classificate(temp, data).equals(data.getAttributes().get(size-1))){
+                rightValue = rightValue + 1;
+            }
+        }
+        out.println(rightValue/dataset.getDataSet().size());
+    }
+    
 }
